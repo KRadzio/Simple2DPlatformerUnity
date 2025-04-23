8da0f8749af8baa554781b025f5857b55a43779f
KRadzio <rakradek@yahoo.com>
2025-04-23 17:37:37 +0200

    added files

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void OnLevel1ButtonPressed()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void OnExitToDesktopButtonPressed()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+        Application.Quit();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
